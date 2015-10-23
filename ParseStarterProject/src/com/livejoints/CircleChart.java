3972ae26a6a53206e58bcda939745d770d9e9eea
nathantofte <nathan.tofte@gmail.com>
2015-10-22 22:42:46 -0500

    New arm chart (limbDisplay) updates position based on sensor.

--- ParseStarterProject/src/com/livejoints/CircleChart.java
+++ ParseStarterProject/src/com/livejoints/CircleChart.java
@@ -7,12 +7,23 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 /**
  * Created by nathantofte on 10/20/15.
  */
 public class CircleChart extends ImageView {
+    private final static String TAG = CircleChart.class.getSimpleName();
+
+    int valuesByTens[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int adjustedValuesByTens[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    long valuesTotal = 0;
+
+    private final int NUMBER_OF_CATEGORIES = 35;
+    Paint paint;
+
+
     public CircleChart(Context context) {
         super(context);
         paint = new Paint();
@@ -25,13 +36,63 @@ public class CircleChart extends ImageView {
         setBackgroundColor(Color.TRANSPARENT);
     }
 
-    Paint paint;
+
+
+
+    private void printValues() {
+        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+            Log.d(TAG, "" + i + ": " + valuesByTens[i]);
+        }
+    }
+
+    final long MAX_VALUES_TOTAL = 50000;
+
+    private void alignValues() {
+        if (valuesTotal > MAX_VALUES_TOTAL) {
+            valuesTotal = valuesTotal / 100;
+
+            for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+                valuesByTens[i] = valuesByTens[i] / 100;
+            }
+        }
+    }
+
+
+    final static int CLIP_VALUE = 100;
+    // ok with 100% in each of the 36 categories.  So lets set bar at valuesTotal / 36.  Then get percent form that
+
+    private void adjustedValues() {
+        double saturationValue = (double)valuesTotal / (double)NUMBER_OF_CATEGORIES;
+        if (saturationValue < 100) saturationValue = 100;
+
+        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+            double currentValue = (double) valuesByTens[i];
+
+
+            double percent = (currentValue / saturationValue) * 100.0;
+            if (percent > CLIP_VALUE) percent = CLIP_VALUE;
+
+            adjustedValuesByTens[i] = (int) percent;
+        }
+
+    }
+
+    private void printAdjustedValues() {
+        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
+            Log.d(TAG, "adjusted " + i + ": " + adjustedValuesByTens[i]);
+        }
+    }
+
+
+
+
+
 
     @Override
     protected void onDraw(Canvas canvas) {
 
         super.onDraw(canvas);
-        drawArcSegment(canvas,getWidth()/2, getHeight()/2, 50.0f, 130.0f, 10.0f , 10.0f, paint, null);
+        drawArcSegment(canvas, getWidth() / 2, getHeight() / 2, 50.0f, 130.0f, 10.0f, 10.0f, paint, null);
     }
 
 
--- ParseStarterProject/src/com/livejoints/fragments/LimbDisplayFragment.java
+++ ParseStarterProject/src/com/livejoints/fragments/LimbDisplayFragment.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.github.mikephil.charting.charts.PieChart;
 import com.livejoints.R;
 import com.livejoints.bluetooth.BluetoothLeService;
 
@@ -23,12 +22,7 @@ import com.livejoints.bluetooth.BluetoothLeService;
 public class LimbDisplayFragment extends Fragment {
     private final static String TAG = LimbDisplayFragment.class.getSimpleName();
 
-    int valuesByTens[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int adjustedValuesByTens[] = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    long valuesTotal = 0;
-    private PieChart mChart;
 
-    private final int NUMBER_OF_CATEGORIES = 35;
 
     ImageView upperArm;
     ImageView lowerArm;
@@ -40,10 +34,6 @@ public class LimbDisplayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_limb_display, container, false);
-
-        mChart = (PieChart) v.findViewById(R.id.chart);
-
-        printValues();
 
         upperArm = (ImageView)v.findViewById(R.id.upperArmImageView);
         lowerArm = (ImageView)v.findViewById(R.id.lowerArmImageView);
@@ -64,50 +54,6 @@ public class LimbDisplayFragment extends Fragment {
 
     private void setAngle(int angle) {
         lowerArm.setRotation(angle);
-    }
-
-    private void printValues() {
-        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
-            Log.d(TAG, "" + i + ": " + valuesByTens[i]);
-        }
-    }
-
-    final long MAX_VALUES_TOTAL = 50000;
-
-    private void alignValues() {
-        if (valuesTotal > MAX_VALUES_TOTAL) {
-            valuesTotal = valuesTotal / 100;
-
-            for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
-                valuesByTens[i] = valuesByTens[i] / 100;
-            }
-        }
-    }
-
-
-    final static int CLIP_VALUE = 100;
-    // ok with 100% in each of the 36 categories.  So lets set bar at valuesTotal / 36.  Then get percent form that
-
-    private void adjustedValues() {
-        double saturationValue = (double)valuesTotal / (double)NUMBER_OF_CATEGORIES;
-        if (saturationValue < 100) saturationValue = 100;
-
-        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
-            double currentValue = (double) valuesByTens[i];
-
-
-            double percent = (currentValue / saturationValue) * 100.0;
-            if (percent > CLIP_VALUE) percent = CLIP_VALUE;
-
-            adjustedValuesByTens[i] = (int) percent;
-        }
-
-    }
-
-    private void printAdjustedValues() {
-        for (int i = 0; i < NUMBER_OF_CATEGORIES; i++) {
-            Log.d(TAG, "adjusted " + i + ": " + adjustedValuesByTens[i]);
-        }
     }
 
     @Override
@@ -163,17 +109,14 @@ public class LimbDisplayFragment extends Fragment {
                         int angle = Integer.parseInt(angleStr);
 
                         setAngle(angle);
-                        int index = angle / 10;
-                        if (index > NUMBER_OF_CATEGORIES) index = NUMBER_OF_CATEGORIES;
-
-                        valuesByTens[index]++;
-                        valuesTotal++;
-                        alignValues();
-                        adjustedValues();
+//                        int index = angle / 10;
+//                        if (index > NUMBER_OF_CATEGORIES) index = NUMBER_OF_CATEGORIES;
+//
+//                        valuesByTens[index]++;
+//                        valuesTotal++;
+//                        alignValues();
+//                        adjustedValues();
                         //printAdjustedValues();
-                        //printValues();
-                        //Log.d(TAG, "data received int =========>" + angle);
-                        //armAngle(angle);
 
                     } catch (NumberFormatException nfe) {
                         Log.d(TAG, "bummer.  value for angle was wierd: " + angleStr);
