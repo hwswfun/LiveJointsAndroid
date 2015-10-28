2d1131b91081578b35855d736b854aeb4cf23fa9
nathantofte <nathan.tofte@gmail.com>
2015-10-28 07:08:19 -0500

    Chart working well.  Now need better graphics:).

--- ParseStarterProject/src/com/livejoints/CircleChart.java
+++ ParseStarterProject/src/com/livejoints/CircleChart.java
@@ -24,21 +24,123 @@ public class CircleChart extends ImageView {
     long valuesTotal = 0;
 
     private final int NUMBER_OF_CATEGORIES = 35;
+
     Paint paint;
+    Paint overlayPaint;
+    Paint maskoffPaint;
+
+    // chartCanvas is the normal draw from this ImageView.  Instead of drawing directly into the
+    // canvas, it is drawn into chartCanvas then masked off using transparencyMaskCanvas
+    Bitmap chartBitmap;
+    Canvas chartCanvas;
+
+    Bitmap transparencyMaskBitmap;
+    Canvas transparencyMaskCanvas;
 
 
     public CircleChart(Context context) {
         super(context);
-        paint = new Paint();
+        init();
     }
 
     public CircleChart(Context context, AttributeSet attrs) {
         super(context, attrs);
-        paint = new Paint();
-        paint.setColor(Color.CYAN);
-        setBackgroundColor(Color.GREEN);
         init();
     }
+
+
+    void init() {
+        setBackgroundColor(Color.TRANSPARENT);
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+
+        maskoffPaint = new Paint();
+        maskoffPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        maskoffPaint.setAntiAlias(true);
+
+        overlayPaint = new Paint();
+        overlayPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+        overlayPaint.setAntiAlias(true);
+    }
+
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        if (w != oldw || h != oldh) {
+            transparencyMaskBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            transparencyMaskCanvas = new Canvas(transparencyMaskBitmap);
+            chartBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            chartCanvas = new Canvas(chartBitmap);
+        }
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // draws the src image for this into the chartCanvas
+        super.onDraw(chartCanvas);
+
+        transparencyMaskBitmap.eraseColor(Color.TRANSPARENT);
+
+        // draw the chart as an opaque color with transparent non-chart part.
+        drawGraph(transparencyMaskCanvas);
+
+        // non transparent colors in transparencyMaskCanvas allow the original src "chartCanvas" to
+        // show through.  any points transparent in transparencyMaskCanvas become transparent.
+        chartCanvas.drawBitmap(transparencyMaskBitmap, 0, 0, maskoffPaint);
+
+        canvas.drawBitmap(chartBitmap, 0, 0, overlayPaint);
+    }
+
+
+    void drawGraph(Canvas canvas) {
+        int centerX = getWidth() / 2;
+        int centerY = getHeight() / 2;
+        int radiusOutside = 150;
+        int radiusInside = 40;
+        int sweepAngle = 10;
+
+        RectF outerRect; // = new RectF(centerX - radiusOutside, centerY - radiusOutside, centerX + radiusOutside, centerY + radiusOutside);
+        RectF innerRect = new RectF(centerX - radiusInside, centerY - radiusInside, centerX + radiusInside, centerY + radiusInside);
+
+        int startAngle = 270;
+        int prevAngle = startAngle;
+
+
+        Path segmentPath = new Path();
+        double start = Math.toRadians(startAngle);
+
+        segmentPath.moveTo((float) (centerX + radiusInside * Math.cos(start)), (float) (centerY + radiusInside * Math.sin(start)));
+
+        for (int i = 0; i < NUMBER_OF_CATEGORIES-20; i++) {
+            //Log.d(TAG, "adjusted " + i + ": " + adjustedValuesByTens[i]);
+
+            radiusOutside = 70 + adjustedValuesByTens[i];
+            sweepAngle = 10;
+
+            outerRect = new RectF(centerX - radiusOutside, centerY - radiusOutside, centerX + radiusOutside, centerY + radiusOutside);
+            segmentPath.arcTo(outerRect, prevAngle, sweepAngle);
+            prevAngle = prevAngle + sweepAngle;
+        }
+
+        int currentAngle = startAngle + prevAngle + sweepAngle;
+
+        //double end = Math.toRadians(prevAngle + sweepAngle);
+        //segmentPath.lineTo((float)(centerX + radiusInside * Math.cos(end)), (float)(centerY + radiusInside * Math.sin(end)));
+        segmentPath.arcTo(innerRect, currentAngle, -currentAngle);
+
+        canvas.drawPath(segmentPath, paint);
+    }
+
+
+
+
+
+
 
     public void addValue(int angle) {
         int index = angle / 10;
@@ -98,96 +200,10 @@ public class CircleChart extends ImageView {
         }
     }
 
-    Paint eraser;
-    Bitmap transparencyMaskBitmap;
-    Canvas transparencyMaskCanvas;
-    Bitmap chartBitmap;
-    Canvas chartCanvas;
 
 
-    void init() {
-        eraser = new Paint();
-        eraser.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        //eraser.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        eraser.setAntiAlias(true);
-    }
 
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        if (w != oldw || h != oldh) {
-            transparencyMaskBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            transparencyMaskCanvas = new Canvas(transparencyMaskBitmap);
-            chartBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            chartCanvas = new Canvas(chartBitmap);
-        }
-        super.onSizeChanged(w, h, oldw, oldh);
-    }
-
-Paint mypaint;
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-this.setBackgroundColor(Color.RED);
-        setBackground(getResources().getDrawable(R.drawable.ic_launcher));
-
-        //setBackground(R.drawable.ic_launcher);
-        canvas.drawColor(Color.TRANSPARENT);
-        super.onDraw(chartCanvas);
-        Log.d(TAG, "onDraw()");
-        transparencyMaskCanvas.drawColor(Color.TRANSPARENT);
-        drawGraph(transparencyMaskCanvas);
-        chartCanvas.drawBitmap(transparencyMaskBitmap, 0, 0, eraser);
-        //drawArcSegment(canvas, getWidth() / 2, getHeight() / 2, 50.0f, 130.0f, 10.0f, 10.0f, paint, null);
-
-
-        mypaint = new Paint();
-        mypaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
-        //eraser.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        mypaint.setAntiAlias(true);
-        canvas.drawBitmap(chartBitmap, 0, 0, mypaint);
-
-    }
-
-
-    void drawGraph(Canvas canvas) {
-        int centerX = getWidth() / 2;
-        int centerY = getHeight() / 2;
-        int radiusOutside = 150;
-        int radiusInside = 1;
-        int sweepAngle = 10;
-
-        RectF outerRect; // = new RectF(centerX - radiusOutside, centerY - radiusOutside, centerX + radiusOutside, centerY + radiusOutside);
-        RectF innerRect = new RectF(centerX - radiusInside, centerY - radiusInside, centerX + radiusInside, centerY + radiusInside);
-
-        int startAngle = 270;
-        int prevAngle = startAngle;
-
-
-        Path segmentPath = new Path();
-        double start = Math.toRadians(startAngle);
-
-        segmentPath.moveTo((float) (centerX + radiusInside * Math.cos(start)), (float) (centerY + radiusInside * Math.sin(start)));
-
-        for (int i = 0; i < NUMBER_OF_CATEGORIES-20; i++) {
-            //Log.d(TAG, "adjusted " + i + ": " + adjustedValuesByTens[i]);
-
-            radiusOutside = 60 + adjustedValuesByTens[i];
-            sweepAngle = 10;
-
-            outerRect = new RectF(centerX - radiusOutside, centerY - radiusOutside, centerX + radiusOutside, centerY + radiusOutside);
-            segmentPath.arcTo(outerRect, prevAngle, sweepAngle);
-            prevAngle = prevAngle + sweepAngle;
-        }
-
-        int currentAngle = startAngle + prevAngle + sweepAngle;
-
-        //double end = Math.toRadians(prevAngle + sweepAngle);
-        //segmentPath.lineTo((float)(centerX + radiusInside * Math.cos(end)), (float)(centerY + radiusInside * Math.sin(end)));
-        segmentPath.arcTo(innerRect, currentAngle, -currentAngle);
-
-        canvas.drawPath(segmentPath, paint);
-    }
 
 }
 
